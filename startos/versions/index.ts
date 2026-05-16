adee01f58c9b71dcee207f3e291d13356162fb8e
Helix <helix@start9.com>
2026-05-16 15:08:30 +0000

    chore: bump start-sdk to 1.5.2
    Bumps `@start9labs/start-sdk` 1.5.1 → 1.5.2 and the package
    revision to 2.0.10888:5. 1.5.2 fixes empty `withPgDump` /
    `withMysqlDump` backups (not used by this package). Upstream
    Jitsi (`stable-10888`) and coturn (`4.11.0`) are already current.

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_2_0_10888_4 } from './v2.0.10888.4'
+import { v_2_0_10888_5 } from './v2.0.10888.5'
 
 export const versionGraph = VersionGraph.of({
-  current: v_2_0_10888_4,
+  current: v_2_0_10888_5,
   other: [],
 })
